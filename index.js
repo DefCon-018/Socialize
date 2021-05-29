b57338dd0eaec9b24c7d4b5b56ab9caa39d162ec
DefCon-018 <hp761570@gmil.com>
2021-05-29 17:21:58 +0530

    Create partials and views

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
-app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static('assets'));
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
