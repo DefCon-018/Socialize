8fd218c020fa9414e76072d4e74f58ebf457d330
DefCon-018 <hp761570@gmil.com>
2021-06-06 17:37:25 +0530

    Modify chat engine

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,8 +1,9 @@
 
 class ChatEngine{
-    constructor(chatBoxId, userEmail){
+    constructor(chatBoxId, userEmail, name){
         this.chatBoxId = $(`.${chatBoxId}`);
         this.userEmail = userEmail;
+        this.name = name;
         this.socket = io.connect('http://localhost:5000');
 
         if(this.userEmail){
@@ -15,41 +16,46 @@ class ChatEngine{
         this.socket.on('connect', function(){
             console.log("connection established using socket...!");
 
-            self.socket.emit('join_room',{
+            self.socket.on('username', {
                 email: self.userEmail,
-                chatRoom: 'socialize'
+                name: self.name
             })
 
-            self.socket.on('user_joined', function(data){
-                console.log("User joined", data);
-            })
+            // self.socket.emit('join_room',{
+            //     email: self.userEmail,
+            //     chatRoom: 'socialize'
+            // })
 
-            $('#message-send').click(function(e){
-                e.preventDefault();
-                let msg = $('#chat-message').val();
-                $('#chat-message').val('');
-                if(msg != ''){
-                    self.socket.emit('send_message', {
-                        message: msg,
-                        userEmail: self.userEmail,
-                        chatRoom: 'socialize'
-                    })
-                }
-            })
+            // self.socket.on('user_joined', function(data){
+            //     console.log("User joined", data);
+            // })
 
-            self.socket.on('recieve_message', function(data){
-                console.log(data.message);
-                let newMessage = $('<div>');
-                let messageType = 'others-message';
-                if(self.userEmail == data.userEmail){
-                    messageType = 'my-message'
-                }
-                $(newMessage).append($('<span>', {
-                    'html': data.message
-                }));
-                $(newMessage).addClass(messageType);
-                $('#chat-box-body').append(newMessage);
-            })
+            // $('#message-send').click(function(e){
+            //     e.preventDefault();
+            //     let msg = $('#chat-message').val();
+            //     $('#chat-message').val('');
+            //     if(msg != ''){
+            //         self.socket.emit('send_message', {
+            //             message: msg,
+            //             userEmail: self.userEmail,
+            //             chatRoom: 'socialize'
+            //         })
+            //     }
+            // })
+
+            // self.socket.on('recieve_message', function(data){
+            //     console.log(data.message);
+            //     let newMessage = $('<div>');
+            //     let messageType = 'others-message';
+            //     if(self.userEmail == data.userEmail){
+            //         messageType = 'my-message'
+            //     }
+            //     $(newMessage).append($('<span>', {
+            //         'html': data.message
+            //     }));
+            //     $(newMessage).addClass(messageType);
+            //     $('#chat-box-body').append(newMessage);
+            // })
             
         })
     }
