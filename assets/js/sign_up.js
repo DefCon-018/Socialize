a5c4d8f7f0a9f0c39e3c93bf499ec5c2c5e312ad
DefCon-018 <hp761570@gmil.com>
2021-06-09 18:23:54 +0530

    Make login and signup pages better

--- assets/js/sign_up.js
+++ assets/js/sign_up.js
@@ -1,22 +1,23 @@
 let login = document.getElementById('login');
 let register= document.getElementById('register');
-let btn = document.querySelector('.btn');
+
 let loginBtn = document.getElementById('btn-login');
 let signupBtn = document.getElementById('btn-signup');
 
 function handleRegister(){
-    register.style.left = "100px";
-    login.style.left = "-550px";
-    btn.style.left= "50%"
-    btn.style.right = "0px"
-    signupBtn.style.color= "white";
-    loginBtn.style.color = "blue"
+    register.style.left = "57px";
+    login.style.left = '-560px';
+    signupBtn.style.borderBottomColor ='blue';
+    signupBtn.style.borderBottomWidth= "4px";
+    signupBtn.style.borderBlockEndStyle = "solid"
+    loginBtn.style.borderBottom = "3px solid lightgray";
 }
 
 function handleLogin(){
     register.style.left = "560px";
-    login.style.left = '100px';
-    btn.style.left = "0px";
-    loginBtn.style.color = "white";
-    signupBtn.style.color = "blue";
+    login.style.left = '57px';
+    loginBtn.style.borderBottomColor ='blue';
+    loginBtn.style.borderBottomWidth= "4px";
+    loginBtn.style.borderBlockEndStyle = "solid"
+    signupBtn.style.borderBottom = "3px solid lightgray";
 }
