61368ee0b15222a6209ae29002076f57f7fdf3a2
DefCon-018 <hp761570@gmil.com>
2021-06-09 18:37:26 +0530

    make signin signup page responsive

--- assets/js/sign_in.js
+++ assets/js/sign_in.js
@@ -6,18 +6,22 @@ let signupBtn = document.getElementById('btn-signup');
 
 function handleRegister(){
     register.style.left = "57px";
-    login.style.left = '-560px';
+    login.style.left = '-620px';
     signupBtn.style.borderBottomColor ='blue';
     signupBtn.style.borderBottomWidth= "4px";
     signupBtn.style.borderBlockEndStyle = "solid"
     loginBtn.style.borderBottom = "3px solid lightgray";
+    signupBtn.style.color= "blue";
+    loginBtn.style.color= "gray";
 }
 
 function handleLogin(){
-    register.style.left = "560px";
+    register.style.left = "750px";
     login.style.left = '57px';
     loginBtn.style.borderBottomColor ='blue';
     loginBtn.style.borderBottomWidth= "4px";
     loginBtn.style.borderBlockEndStyle = "solid"
     signupBtn.style.borderBottom = "3px solid lightgray";
+    loginBtn.style.color= "blue";
+    signupBtn.style.color= "gray";
 }
--- assets/js/sign_up.js
+++ assets/js/sign_up.js
@@ -11,6 +11,8 @@ function handleRegister(){
     signupBtn.style.borderBottomWidth= "4px";
     signupBtn.style.borderBlockEndStyle = "solid"
     loginBtn.style.borderBottom = "3px solid lightgray";
+    signupBtn.style.color= "blue";
+    loginBtn.style.color= "gray";
 }
 
 function handleLogin(){
@@ -20,4 +22,6 @@ function handleLogin(){
     loginBtn.style.borderBottomWidth= "4px";
     loginBtn.style.borderBlockEndStyle = "solid"
     signupBtn.style.borderBottom = "3px solid lightgray";
+    loginBtn.style.color= "blue";
+    signupBtn.style.color= "gray";
 }
